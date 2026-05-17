0c7e9c4d005210f9f444ade8e6ff655231695c9b
Daniel Favour Oshidero <dfoshidero@outlook.com>
2026-05-17 15:06:46 +0100

    fix instagram api endpoint

--- src/content/portfolio/Portfolio.jsx
+++ src/content/portfolio/Portfolio.jsx
@@ -23,7 +23,7 @@ function PortfolioCard() {
         try {
           attempts++;
           const response = await axios.get(
-            "https://z3mlw599i2.execute-api.eu-west-2.amazonaws.com/test/fetchInstagramData"
+            "https://z3mlw599i2.execute-api.eu-west-2.amazonaws.com/dev/fetchInstagramData"
           );
           const imagesData = JSON.parse(response.data.body); // Parse the JSON string into an array
 
